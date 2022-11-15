2b25cfbd595a010f0845b645732bdc1dae2c91be
andikhusairi <ahmad_andi_khusairi@teknokrat.ac.id>
2022-11-15 20:04:01 +0700

    Tugas Pemrograman Mobile 2

---
+++ app/src/main/java/com/alifdanangp/ui/theme/Type.kt
@@ -0,0 +1,18 @@
+package com.alifdanangp.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.alifdanangp.R
+
+
+val montserratFamily = FontFamily(
+    Font(R.font.montserrat_light, FontWeight.Light),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
+
+val Typography = Typography(
+    defaultFontFamily = montserratFamily
+)
